d68a858d33098349c50c6fdd52044600e206e983
Brett Slaski <brettski@yahoo.com>
2018-09-28 23:46:05 -0500

    Initial Commit

---
+++ EntLibCryptoCli/Options/General.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine;
+
+namespace EntLibCryptoCli.Options
+{
+    /// <summary>
+    /// Defines command line options
+    /// </summary>
+    public class General
+    {
+        [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+        public bool Verbose { get; set; }
+
+        /* I don't think this is needed
+        [Option('h', "help", Required = false, HelpText = "Desplays help infomation.")]
+        public bool Help { get; set; }
+        */
+    }
+}
---
+++ EntLibCryptoCli/Options/Import.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine;
+
+namespace EntLibCryptoCli.Options
+{
+    [Verb("import", HelpText = "Import a previously exported provider key.")]
+    public class ImportOptions
+    {
+
+        [Option('i', "importfile", Required = true, HelpText = "Previously exported key to be imported")]
+        public string ImportFile { get; set; }
+
+        [Option('o', "outputkey", Required = true, HelpText = "New symmetric key file to save")]
+        public string OutputKeyFile { get; set; }
+
+        [Option('p', "password", Required = true, HelpText = "Password used to secure previously exported key")]
+        public string Password { get; set; }
+    }
+}
