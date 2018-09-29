f88647ed1191c48646802340c7c7d83c6784b1d5
Brett Slaski <brettski@yahoo.com>
2018-09-29 00:12:43 -0500

    Add export options and Cryptography block library from NuGet

---
+++ EntLibCryptoCli/Options/Export.cs
@@ -0,0 +1,17 @@
+﻿using CommandLine;
+
+namespace EntLibCryptoCli.Options
+{
+    [Verb("export", HelpText = "Export a current symmetric key so it may be transferred to a new computer.")]
+    public class Export
+    {
+        [Option('k', "keyfile", Required = true, HelpText = "Current working Symmetric key to export")]
+        public string KeyFile { get; set; }
+
+        [Option('p', "password", Required = true, HelpText = "Password to protext exported key. Min 8, 1 punctuation")]
+        public string Password { get; set; }
+
+        [Option('e', "exportkey", Required = true, HelpText = "Location and name for exported key")]
+        public string ExportKey { get; set; }
+    }
+}
