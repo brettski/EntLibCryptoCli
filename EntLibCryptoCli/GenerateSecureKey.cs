780b3b70a65b9ca603c12b69b80b6c9a6967a96e
Henry Padilla <henry.padilla@wolterskluwer.com>
2021-02-08 21:25:23 +0000

    Adding GenerateKey to functionality

---
+++ EntLibCryptoCli/GenerateSecureKey.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Security.Cryptography;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace EntLibCryptoCli.Service
+{
+    public static class GenerateSecureKey
+    {
+        /// <summary>
+        /// Restores a Cryptographic key from previously exported file. Use to transfer key to new computer
+        /// </summary>
+        /// <param name="ImportFile">Previously exported symmetric key to be restored</param>
+        /// <param name="Password">Password used to secure previously exported key</param>
+        /// <param name="NewProviderKey">Location and filename of key to be restored.</param>
+        /// <returns></returns>
+        public static bool Generate(string Password, FileInfo NewKeyFile)
+        {
+            KeyManager.ClearCache();
+
+            byte[] managerByteKey = KeyManager.GenerateSymmetricKey("Rijndael");
+            ProtectedKey newProtectedKey = ProtectedKey.CreateFromPlaintextKey(managerByteKey, DataProtectionScope.LocalMachine);
+
+            using (FileStream ofs = File.OpenWrite(NewKeyFile.FullName))
+            {
+                KeyManager.ArchiveKey(ofs, newProtectedKey, Password);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ EntLibCryptoCli/Options/GenerateKey.cs
@@ -0,0 +1,14 @@
+﻿using CommandLine;
+
+namespace EntLibCryptoCli.Options
+{
+    [Verb("generatekey", HelpText = "Generates a RijndaelManaged key using System.Security.Cryptography and saves it to a password protected file that can be restored to different machines.")]
+    public class GenerateKey
+    {
+        [Option('k', "keyfile", Required = true, HelpText = "File location and name where key will be exported.")]
+        public string KeyFile { get; set; }
+
+        [Option('p', "password", Required = true, HelpText = "Password used to secure exported key.")]
+        public string Password { get; set; }
+    }
+}
